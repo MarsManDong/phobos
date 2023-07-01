8ecc7093f5a8207b9a470e063932ca6b54b0f085
biantaidong <marsman@biantaidongdeMacBook-Pro.local>
2023-07-02 01:00:53 +0800

    初始化

---
+++ src/main/java/com/mars/phobos/PhobosApplication.java
@@ -0,0 +1,13 @@
+package com.mars.phobos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhobosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhobosApplication.class, args);
+	}
+
+}
